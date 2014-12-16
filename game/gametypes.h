a6e1319fad0db97b3bde5c0cf2c9d4fc1712a397
Marc Di Luzio <mdiluzio@feralinteractive.com>
2014-12-16 13:13:00 +0000

    Add invalid coord variables

--- game/gametypes.h
+++ game/gametypes.h
@@ -1,6 +1,8 @@
 #ifndef _GAME_TYPES_H_
 #define _GAME_TYPES_H_
 
+#include <limits>       // std::numeric_limits
+
 // Type for a team IDs
 typedef unsigned short team_id_t;
 
@@ -15,5 +17,12 @@ typedef char unitType_c;
 
 // Typedef for unit visual representations
 typedef char unitVis_c;
+
+// Invalid data for above types
+static const team_id_t 		team_id_invalid 	= std::numeric_limits<team_id_t>::max();
+static const player_id_t 	player_id_invalid 	= std::numeric_limits<player_id_t>::max();
+static const unit_id_t 		unit_id_invalid 	= std::numeric_limits<unit_id_t>::max();
+static const unitType_c 	unitType_invalid 	= std::numeric_limits<unitType_c>::max();
+static const unitVis_c 		unitVis_invalid 	= std::numeric_limits<unitVis_c>::max();
 
 #endif //_GAME_TYPES_H_
--- maths/mathtypes.h
+++ maths/mathtypes.h
@@ -1,9 +1,15 @@
 #ifndef _BASETYPES_H_
 #define _BASETYPES_H_
 
+#include <limits>       // std::numeric_limits
+
 // Coordinate types
 typedef short 			coord_t;
 typedef unsigned short 	ucoord_t;
+
+// Invalid values
+static const coord_t 		coord_invalid 	= std::numeric_limits<coord_t>::max();
+static const ucoord_t 		ucoord_invalid 	= std::numeric_limits<ucoord_t>::max();
 
 // Direction representation
 enum dir_t : char
