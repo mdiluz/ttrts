00579863804401a242c032d945d042d9f1009974
Marc Di Luzio <mdiluzio@feralinteractive.com>
2014-12-16 13:12:57 +0000

    header game types

---
+++ game/gametypes.h
@@ -0,0 +1,13 @@
+#ifndef _GAME_TYPES_H_
+#define _GAME_TYPES_H_
+
+// Type for a team IDs
+typedef unsigned short team_id_t;
+
+// Type for player IDs
+typedef unsigned short player_id_t;
+
+// Type for unit IDs
+typedef unsigned short unit_id_t;
+
+#endif //_GAME_TYPES_H_
