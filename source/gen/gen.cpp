b37fe327f2346a6b02dd7a1126065608d6d25a87
mdiluzio <thefoips@gmail.com>
2014-12-20 15:35:19 +0000

    Re-do generation now that attack rule has changed

--- source/gen/gen.cpp
+++ source/gen/gen.cpp
@@ -25,28 +25,27 @@ int main()
 {
     // Tiny 2v2 Game
     {
-        CTTRTSGame game(7, 5);
+        CTTRTSGame game(6, 6);
         game.SetName("Tiny2v2");
 
-        AddUnitToGame( Team::Blue, '>', uvector2(1, 1), game);
-        AddUnitToGame( Team::Blue, '>', uvector2(1, 3), game);
-
-        AddUnitToGame( Team::Red, '<', uvector2(5, 1), game);
-        AddUnitToGame( Team::Red, '<', uvector2(5, 3), game);
+        AddUnitToGame( Team::Red, '<', uvector2(4, 2), game);
+        AddUnitToGame( Team::Red, '<', uvector2(4, 4), game);
+        AddUnitToGame( Team::Green, '>', uvector2(1, 1), game);
+        AddUnitToGame( Team::Green, '>', uvector2(1, 3), game);
 
         OutputGame(std::move(game));
     }
 
     // Basic 5v5 game
     {
-        CTTRTSGame game(21, 11);
-        game.SetName("Basic5v5");
+        CTTRTSGame game(20, 12);
+        game.SetName("Big2v2");
 
+        for ( ucoord_t y : { 2,4,6,8,10 } )
+            AddUnitToGame( Team::Red, '<', uvector2(18, y), game);
         for ( ucoord_t y : { 1,3,5,7,9 } )
-            AddUnitToGame( Team::Blue, '>', uvector2(1, y), game);
+            AddUnitToGame( Team::Green, '>', uvector2(1, y), game);
 
-        for ( ucoord_t y : { 1,3,5,7,9 } )
-            AddUnitToGame( Team::Red, '<', uvector2(19, y), game);
 
         OutputGame(std::move(game));
     }
@@ -56,14 +55,14 @@ int main()
         CTTRTSGame game(8, 8);
         game.SetName("Chess");
 
-        for ( ucoord_t y : { 0,1,2,3,4,5,6,7 } ) {
-            AddUnitToGame(Team::Blue, '>', uvector2(0, y), game);
-            AddUnitToGame(Team::Blue, '>', uvector2(1, y), game);
-        }
-
-        for ( ucoord_t y : { 0,1,2,3,4,5,6,7 } ) {
+        for ( ucoord_t y : { 1,3,5,7 } ) {
             AddUnitToGame(Team::Red, '<', uvector2(6, y), game);
             AddUnitToGame(Team::Red, '<', uvector2(7, y), game);
+        }
+
+        for ( ucoord_t y : { 0,2,4,6 } ) {
+            AddUnitToGame(Team::Green, '>', uvector2(0, y), game);
+            AddUnitToGame(Team::Green, '>', uvector2(1, y), game);
         }
 
         OutputGame(std::move(game));
