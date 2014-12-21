c38fb3dd41dd178285050e3e7ab42bfa595346b3
mdiluzio <thefoips@gmail.com>
2014-12-21 10:41:06 +0000

    Realign unit.h

--- source/game/unit.h
+++ source/game/unit.h
@@ -18,24 +18,21 @@ class CUnit
 public:
 
 	// Factory function for creating units from a visual
-	static CUnit GetUnitFromVis( unitVis_c vis );
+	static CUnit 		GetUnitFromVis( unitVis_c vis );
 
 	// Unit <--> string conversion functions
-	static std::string GetStringFromUnit(const CUnit& unit );
-	static CUnit GetUnitFromString(const std::string& unit );
+	static std::string 	GetStringFromUnit(const CUnit& unit );
+	static CUnit 		GetUnitFromString(const std::string& unit );
 
     // Constructor
 	CUnit();
 
     // Move constructor and move assignment. CUnit cannot be copied
     CUnit(CUnit&& unit);
-    CUnit& operator=(CUnit&& unit);
+    CUnit& 				operator=(CUnit&& unit);
 
-	bool operator==(const CUnit& rhs);
-	bool operator!=(const CUnit& rhs) { return !(*this == rhs); }
-
-    // Default dtor
-	~CUnit() = default;
+	bool 				operator==(const CUnit& rhs);
+	bool 				operator!=(const CUnit& rhs) { return !(*this == rhs); }
 
     // Getters for all the members
 	inline const unit_id_t& 	getID() const 		{ return unit_id; }
@@ -45,44 +42,44 @@ public:
 	inline const uvector2& 		getPos() const 						{ return pos; }
 
 	// Set
-    inline Team 			setTeam(const Team & v)  		{ return (team_id = v); }
-    inline unitVis_c	 	setVisual(const unitVis_c& v)  	{ return ( unit_vis = v ); }
-    inline dir_t 			setDir(const dir_t& v)          { return (dir = v); }
-	inline void 			setPos(const uvector2& v)  		{ pos = v; }
+    inline Team 				setTeam(const Team & v)  		{ return (team_id = v); }
+    inline unitVis_c	 		setVisual(const unitVis_c& v)  	{ return ( unit_vis = v ); }
+    inline dir_t 				setDir(const dir_t& v)          { return (dir = v); }
+	inline void 				setPos(const uvector2& v)  		{ pos = v; }
 
     // Get the co-ordinate in front of the unit
-    uvector2 		getInFront() const;
+    uvector2 					getInFront() const;
 
 	// Check unit is valid
-	inline bool 	valid() const;
+	inline bool 				valid() const;
 
 	// Set a unit based solely on it's visual
-	bool 			setFromVisual( const unitVis_c& vis);
+	bool 						setFromVisual( const unitVis_c& vis);
 
     // Orientation methods
-    dir_t turnLeft();
-    dir_t turnRight();
-    dir_t turnAround();
+    dir_t 						turnLeft();
+    dir_t 						turnRight();
+    dir_t 						turnAround();
 
 private:
 
     // Update my visual must be called when setting direction
-    unitVis_c 	updateMyVisual();
+    unitVis_c 					updateMyVisual();
 
 	// Unit ID
-    unit_id_t unit_id;
+    unit_id_t 					unit_id;
 
 	// Visual 
-	unitVis_c unit_vis;
+	unitVis_c 					unit_vis;
 
 	// Team ID
-	Team team_id;
+	Team 						team_id;
 
 	// Direction
-	dir_t dir;
+	dir_t 						dir;
 	
 	// Position
-	uvector2 pos;
+	uvector2 					pos;
 };
 
 // Typedef for a vector of units
