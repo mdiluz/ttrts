31644b1f950f563cc7c5cb3807fbf4a98725556b
mdiluzio <thefoips@gmail.com>
2014-12-30 18:33:59 +0000

    Fix a couple of bugs in gen_usage.sh

--- scripts/gen_usage.sh
+++ scripts/gen_usage.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 # Used to generate usage text from markdown
 cat README.md                        \
-    | sed 's/^#* //g'                \
-    | sed 's/\t/\\t/g'               \
+    | sed 's/^#\+ //g'                \
+    | sed 's/^\t/\\t/g'              \
+    | sed 's/^    /\\t/g'            \
     | sed ':a;N;$!ba;s/\n/\\n\n/g'   \
     | sed 's/^/\"/'                  \
     | sed 's/$/\"/'                  \
