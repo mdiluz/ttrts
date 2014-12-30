ad803a5d7af5495a29bd29ab48ece0df0e6dd82a
mdiluzio <thefoips@gmail.com>
2014-12-30 18:53:15 +0000

    Rework bootstrap to use new install targets

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,39 +16,19 @@ if [[ $? != 0 ]]; then
     exit
 fi
 
-# Run make
-echo "TTRTS: Running make"
-make
+echo "Performing install"
+make install
 if [[ $? != 0 ]]; then
-    echo "TTRTS: make failed, exiting Bootstrap"
+    echo "TTRTS: Install failed, check output"
     exit
 fi
 
-# Run tests
+# Run final test to make sure
 echo "TTRTS: Running tests"
 ./source/test/ttrts-test
 if [[ $? != 0 ]]; then
     echo "TTRTS: Tests failed, build must be broken"
     exit
 fi
-
-# Generate maps
-echo "TTRTS: Generating maps"
-cd ..
-./scripts/gen_maps.sh "$PWD/build/source/gen/ttrts-gen"
-if [[ $? != 0 ]]; then
-    echo "TTRTS: Failed to generate maps, exiting Bootstrap"
-    exit
-fi
-
-# Move binaries
-echo "TTRTS: Moving binaries"
-if [ ! -e build/source/client/ttrts ]; then
-    echo "TTRTS: No TTRTS Binary found, something has gone wrong"
-    exit
-fi
-
-cp build/source/client/ttrts .
-chmod a+x ttrts
 
 echo "TTRTS: Bootstrap complete"
