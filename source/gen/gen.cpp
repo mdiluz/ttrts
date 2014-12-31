189241853b6ece5850205f66167b141659db4560
mdiluzio <thefoips@gmail.com>
2014-12-31 18:39:58 +0000

    Update the map generation
    New maps! 3 player!
    Update names, as it wasn't 2v2 so much as 1v1 with 2 units each

--- source/gen/gen.cpp
+++ source/gen/gen.cpp
@@ -24,23 +24,44 @@ void OutputGame( CTTRTSGame&& game )
 
 int main()
 {
-    // Tiny 2v2 Game
+    // Tiny 1v1 Game
+    //------
+    //-G----
+    //----R-
+    //-G----
+    //----R-
+    //------
     {
         CTTRTSGame game(6, 6);
-        game.SetName("Tiny2v2");
+        game.SetName("Tiny2player");
 
         AddUnitToGame( player_t::Red, '<', uvector2(4, 2), game);
         AddUnitToGame( player_t::Red, '<', uvector2(4, 4), game);
         AddUnitToGame( player_t::Green, '>', uvector2(1, 1), game);
         AddUnitToGame( player_t::Green, '>', uvector2(1, 3), game);
 
+        game.AddWall(uvector2(3,2));
+        game.AddWall(uvector2(3,3));
+
         OutputGame(std::move(game));
     }
 
-    // Basic 5v5 game
+    // Basic 1v1 game
+    // --------------------
+    // -G------------------
+    // ------------------R-
+    // -G------------------
+    // ------------------R-
+    // -G------------------
+    // ------------------R-
+    // -G------------------
+    // ------------------R-
+    // -G------------------
+    // ------------------R-
+    // --------------------
     {
         CTTRTSGame game(20, 12);
-        game.SetName("Big2v2");
+        game.SetName("Big2Player");
 
         for ( ucoord_t y : { 2,4,6,8,10 } )
             AddUnitToGame( player_t::Red, '<', uvector2(18, y), game);
@@ -51,7 +72,15 @@ int main()
         OutputGame(std::move(game));
     }
 
-    // Chess 10v10 game
+    // Sort of like Chess
+    //GG------
+    //------RR
+    //GG------
+    //------RR
+    //GG------
+    //------RR
+    //GG------
+    //------RR
     {
         CTTRTSGame game(8, 8);
         game.SetName("Chess");
@@ -65,6 +94,71 @@ int main()
             AddUnitToGame(player_t::Green, '>', uvector2(0, y), game);
             AddUnitToGame(player_t::Green, '>', uvector2(1, y), game);
         }
+
+        OutputGame(std::move(game));
+    }
+
+    // Medium 4 player game
+    //----------
+    //----------
+    //---GGGG---
+    //--R -- B--
+    //--R-  -B--
+    //--R-  -B--
+    //--R -- B--
+    //---YYYY---
+    //----------
+    //----------
+    {
+        CTTRTSGame game(10, 10);
+        game.SetName("Medium4Player");
+
+        for ( ucoord_t y : { 2,3,4,5 } ) {
+            AddUnitToGame(player_t::Red, '>', uvector2(2, y), game);
+            AddUnitToGame(player_t::Blue, '<', uvector2(7, y), game);
+        }
+
+        for ( ucoord_t x : { 2,3,4,5 } ) {
+            AddUnitToGame(player_t::Yellow, '^', uvector2(x,7), game);
+            AddUnitToGame(player_t::Green, 'v', uvector2(x,2), game);
+        }
+
+        // Diagonal walls
+        game.AddWall(uvector2(3,3));
+        game.AddWall(uvector2(3,6));
+        game.AddWall(uvector2(6,3));
+        game.AddWall(uvector2(6,6));
+
+        // middle walls
+        game.AddWall(uvector2(4,4));
+        game.AddWall(uvector2(4,5));
+        game.AddWall(uvector2(5,4));
+        game.AddWall(uvector2(5,5));
+
+        OutputGame(std::move(game));
+    }
+
+    // Medium 3 player game
+    //----------
+    //--------Y-
+    //--------Y-
+    //----------
+    //-G--------
+    //-G--------
+    //----------
+    //--------R-
+    //--------R-
+    //----------
+    {
+        CTTRTSGame game(10, 10);
+        game.SetName("Medium3Player");
+
+        AddUnitToGame(player_t::Red, '<', uvector2(8, 1), game);
+        AddUnitToGame(player_t::Red, '<', uvector2(8, 2), game);
+        AddUnitToGame(player_t::Green, '>', uvector2(1, 4), game);
+        AddUnitToGame(player_t::Green, '>', uvector2(1, 5), game);
+        AddUnitToGame(player_t::Yellow, '<', uvector2(8,7), game);
+        AddUnitToGame(player_t::Yellow, '<', uvector2(8,8), game);
 
         OutputGame(std::move(game));
     }
